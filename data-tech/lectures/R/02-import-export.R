7da38dba00bfbea6bd81da446c7cf190c555e17e
Earo Wang <earo.wang@gmail.com>
2021-01-13 16:54:09 +1300

    added more data fmts

--- data-tech/lectures/R/02-import-export.R
+++ data-tech/lectures/R/02-import-export.R
@@ -8,10 +8,10 @@ lst <- list( # list constructor/creator
 lst
 
 ## ---- lists-type
-typeof(lst)
+typeof(lst) # primitive type
 
 ## ---- lists-cls
-class(lst)
+class(lst) # type + attributes
 
 ## ---- lists-str
 # el can be of diff lengths
@@ -53,68 +53,71 @@ sci_tbl <- tibble(
   percentage = count / sum(count))
 sci_tbl
 
+## ---- tbl-type
+typeof(sci_tbl) # list in essence
+class(sci_tbl) # tibble is a special class of data.frame
+
 ## ---- subsetting
 sci_tbl$count
 sci_tbl[["count"]]
-
-sci_mat <- as.matrix(sci_tbl)
-sci_mat
-as_tibble(sci_mat)
-
-sci_lst <- as.list(sci_tbl)
-sci_lst
-sci_lst$new_column <- sample(1:100, size = 5)
-sci_lst
-as_tibble(sci_lst)
 
 # spreadsheet-like data
 # data src: https://stats.oecd.org/Index.aspx?DataSetCode=TIME_USE#
 # how individuals spend their time
 # read from url
-## excel files
+
+## ---- read-csv
+library(readr) # or library(tidyverse)
+pisa <- read_csv("data/pisa/pisa-student.csv", n_max = 2929621)
+pisa
+
+## ---- vroom
+library(vroom)
+pisa <- vroom("data/pisa/pisa-student.csv", n_max = 2929621)
+
+## ---- excel
 library(readxl)
 time_use <- read_xlsx("data/time-use-oced.xlsx")
+time_use
 
+## ---- skimr
 library(skimr)
 skim(time_use)
-## plain-text rect files
-library(readr)
-## comma delmited files
-pisa <- read_csv("data/pisa/pisa-student.csv")
-write_csv(pisa, file = "data/pisa/pisa-student-copy.csv")
-## fixed width files
-## foreign formats
-# data src: https://www.oecd.org/pisa/data/
+
+## ---- haven
 library(haven)
 pisa2018 <- read_spss("data/pisa/CY07_MSU_STU_QQQ.sav")
 
-## database
-library(DBI)
-con <- dbConnect(RSQLite::SQLite(), dbname = "data/pisa/pisa-student.db")
+## ---- database
+library(RSQLite)
+con <- dbConnect(SQLite(), dbname = "data/pisa/pisa-student.db")
 dbListTables(con)
 dbListFields(con, "pisa")
-pisa <- dbReadTable(con, "pisa", pisa)
+
+## ---- db-read
+pisa <- dbReadTable(con, "pisa")
+
+## ---- db-query
 res <- dbSendQuery(con, "SELECT * FROM pisa WHERE year = 2018")
-pisa2018 <- as_tibble(dbFetch(res))
+pisa2018 <- dbFetch(res)
+
+## ---- db-close
 dbDisconnect(con)
 
-## faster reading
-library(vroom)
-pisa <- vroom("data/pisa/pisa-student.csv")
-
-## larger than memory
+## ---- chunked
 chunked <- function(x, pos) {
   dplyr::filter(x, year == 2018)
 }
 pisa2018 <- read_csv_chunked("data/pisa/pisa-student.csv",
   callback = DataFrameCallback$new(chunked))
 
-## hierarchical data: json
+## ---- json
 library(jsonlite)
 movies <- read_json("data/movies.json")
 length(movies)
 movies[[1]]
 
+## ---- sf
 ## spatial data
 library(sf)
 akl_bus <- st_read("data/BusService/BusService.shp")
