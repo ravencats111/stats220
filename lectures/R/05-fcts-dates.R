4c2241bf4f477c47b9e39541493d9fd4abdce0b1
Earo Wang <earo.wang@gmail.com>
2021-02-02 16:10:44 +1300

    forcats for fcts

--- lectures/R/05-fcts-dates.R
+++ lectures/R/05-fcts-dates.R
@@ -42,3 +42,28 @@ sci_tbl %>%
 movies <- as_tibble(jsonlite::read_json(
   "https://vega.github.io/vega-editor/app/data/movies.json",
   simplifyVector = TRUE))
+
+movies %>% 
+  ggplot(aes(Major_Genre, Rotten_Tomatoes_Rating)) +
+  geom_boxplot()
+
+movies %>% 
+  mutate(
+    Major_Genre = fct_reorder(Major_Genre, Rotten_Tomatoes_Rating,
+      .fun = median, na.rm = TRUE)) %>% 
+  ggplot(aes(Major_Genre, Rotten_Tomatoes_Rating)) +
+  geom_boxplot()
+
+movies %>% 
+  count(Major_Genre) %>% 
+  ggplot(aes(Major_Genre, n)) +
+  geom_col()
+
+movies %>% 
+  mutate(
+    Major_Genre = fct_lump_n(Major_Genre, 5),
+    Major_Genre = fct_reorder(Major_Genre, -n)
+  ) %>% 
+  count(Major_Genre) %>% 
+  ggplot(aes(Major_Genre, n)) +
+  geom_col()
