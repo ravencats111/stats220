840924c0c1edf51718f0b96af1da8f3d83411763
Earo Wang <earo.wang@gmail.com>
2021-02-02 15:12:04 +1300

    started fcts

---
+++ lectures/R/05-fcts-dates.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+dept <- c("Physics", "Mathematics", "Statistics",
+  "Computer Science")
+nstaff <- c(12L, 8L, 20L, 23L)
+sci_tbl <- tibble(dept = dept, count = nstaff)
+sci_tbl
+
+class(dept)
+sort(dept)
+
+(dept_fct <- factor(dept))
+class(dept_fct)
+typeof(dept_fct)
+levels(dept_fct)
+
+factor(dept, levels = dept)
+
+factor(dept, levels = rev(dept))
+
+rep(dept_fct, 3)
+
+library(forcats)
+
+sci_tbl %>% 
+  ggplot(aes(dept, count)) +
+  geom_col()
+
+sci_tbl %>% 
+  mutate(dept = fct_reorder(dept, count)) %>% 
+  pull(dept)
+
+sci_tbl %>% 
+  mutate(dept = fct_reorder(dept, count)) %>% 
+  ggplot(aes(dept, count)) +
+  geom_col()
+
+sci_tbl %>% 
+  mutate(dept = fct_inorder(dept)) %>% 
+  ggplot(aes(dept, count)) +
+  geom_col()
+
+movies <- as_tibble(jsonlite::read_json(
+  "https://vega.github.io/vega-editor/app/data/movies.json",
+  simplifyVector = TRUE))
