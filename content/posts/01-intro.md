666b18577e996b9e9f0427023851159aa3674525
earowang <earo.wang@gmail.com>
2021-03-07 13:03:10 +1300

    uploaded pdf01

--- content/posts/01-intro.md
+++ content/posts/01-intro.md
@@ -5,6 +5,7 @@ img: "https://www.r-project.org/logo/Rlogo.svg"
 slides: "01-intro.html"
 r: "01-intro.R"
 rmd: "01-intro.Rmd"
+pdf: "01-intro.pdf"
 lab:
 sol:
 ---
--- lectures/R/04-data-wrangle.R
+++ lectures/R/04-data-wrangle.R
@@ -10,6 +10,29 @@ time_use <- time_use_raw %>%
     minutes = `Time (minutes)`)
 
 time_use %>% 
+  distinct(category)
+
+time_use %>% 
+  distinct(country) %>% 
+  pull(country)
+
+time_use %>% 
+  slice(1:33)
+
+set.seed(220)
+time_use %>% 
+  slice_sample(n = 10)
+
+time_use %>% 
+  arrange(minutes)
+
+time_use %>% 
+  arrange(desc(minutes)) # -minutes
+
+time_use %>% 
+  arrange(country, desc(minutes))
+
+time_use %>% 
   filter(country %in% c("Australia", "New Zealand"))
 
 time_use %>% 
@@ -77,123 +100,17 @@ time_use %>%
   geom_pointrange(aes(ymin = min, ymax = max), colour = "#e6550d") +
   theme(axis.text.x = element_text(angle = 90))
 
-pisa <- read_csv("data/pisa/pisa-student.csv", n_max = 2929621)
-
-pisa_nz <- pisa %>% 
-  filter(
-    country == "NZL",
-    wealth >= median(wealth, na.rm = TRUE) |
-    !is.na(internet)
-  )
-
-pisa_nz_reorder <- pisa_nz %>% 
-  arrange(desc(year), science)
-
-pisa_nz_select <- pisa_nz_reorder %>% 
-  select(year, gender, internet:escs)
-  # select(-c(country:father_educ, computer))
-
-pisa_nz_mutate <- pisa_nz_select %>% 
-  mutate(
-    average_score = (math + read + science) / 3,
-    internet = if_else(internet == "yes", TRUE, FALSE),
-    year = as.integer(year)
-  )
-
-pisa_nz_mutate %>% 
-  summarise(
-    internet_n = sum(internet, na.rm = TRUE),
-    avg_math = mean(math, na.rm = TRUE),
-    avg_read = mean(read),
-    avg_science = mean(science)
-  )
-
-pisa %>% 
-  filter(
-    country == "NZL",
-    wealth >= median(wealth, na.rm = TRUE) |
-    !is.na(internet)
-  ) %>% 
-  arrange(desc(year), science) %>% 
-  select(year, gender, internet:escs) %>% 
-  mutate(
-    average_score = (math + read + science) / 3,
-    year = as.integer(year)
-  ) %>% 
-  summarise(
-    internet_n = sum(internet == "yes", na.rm = TRUE),
-    avg_math = mean(math, na.rm = TRUE),
-    avg_read = mean(read),
-    avg_science = mean(science)
-  )
-
-pisa_nz %>% 
-  group_by(year)
-
-pisa_nz_byyear <- pisa %>% 
-  filter(country == "NZL") %>% 
-  group_by(year) %>% 
-  summarise(
-    med_math = median(math, na.rm = TRUE),
-    med_read = median(read),
-    med_science = median(science)
-  )
-
-pisa_nz_byyear %>% 
-  ggplot(aes(x = year)) +
-  geom_line(aes(y = med_math)) +
-  geom_point(aes(y = med_math)) +
-  geom_line(aes(y = med_read)) +
-  geom_point(aes(y = med_read)) +
-  geom_line(aes(y = med_science)) +
-  geom_point(aes(y = med_science))
-
-# rename
-pisa_nz %>% 
-  rename(
-    school = school_id,
-    student = student_id
-  )
-
-pisa_nz %>% 
-  transmute(
-    average_score = (math + read + science) / 3,
-    year = as.integer(year)
-  )
-
-# slice
-pisa_nz %>% 
-  slice(1:10, 21:30)
-
-# shortcuts
-pisa_nz %>% 
-  count(gender)
-
-pisa_nz %>% 
-  group_by(gender) %>% 
+time_use_oz %>% 
+  group_by(country) %>% 
   summarise(n = n())
 
-pisa_nz %>% 
-  group_by(gender) %>% 
-  tally()
+# shortcuts
+time_use_oz %>% 
+  count(country) # tally()
 
-# join
-pisa_nzau <- pisa %>% 
-  filter(country %in% c("NZL", "AUS")) %>% 
-  select(year, country, science)
-
-countrycode <- read_csv("data/pisa/countrycode.csv")
-countrycode
-
-pisa_nzau %>% 
-  left_join(countrycode, by = "country")
-
-pisa_nzau %>% 
-  right_join(countrycode, by = "country") %>% 
-  slice_tail(n = 5)
-
-pisa_nzau %>% 
-  anti_join(countrycode, by = "country")
+time_use_oz %>% 
+  group_by(country) %>% 
+  filter(minutes > median(minutes))
 
 # dbplyr
 library(RSQLite)
