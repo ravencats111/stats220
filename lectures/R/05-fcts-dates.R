0ae93504d765ee19ae1d021e2bf76b1d2e63a0f4
earowang <earo.wang@gmail.com>
2021-03-28 22:26:54 +1300

    done with week 5

--- lectures/R/05-fcts-dates.R
+++ lectures/R/05-fcts-dates.R
@@ -51,6 +51,40 @@ sci_tbl %>%
 
 ## ---- fct-vct
 fct_reorder(sci_tbl$dept, sci_tbl$count)
+fct_reorder(sci_tbl$dept, -sci_tbl$count)
+
+## ---- box-movies
+movies <- as_tibble(jsonlite::read_json(
+  "https://vega.github.io/vega-editor/app/data/movies.json",
+  simplifyVector = TRUE))
+ggplot(movies, aes(Rotten_Tomatoes_Rating, Major_Genre)) +
+  geom_boxplot()
+
+## ---- box-movies-med
+movies %>% 
+  mutate(
+    Major_Genre = fct_reorder(
+      Major_Genre, 
+      Rotten_Tomatoes_Rating,
+      .fun = median, na.rm = TRUE)) %>% #<<
+  ggplot(aes(
+    Rotten_Tomatoes_Rating, 
+    Major_Genre)) +
+  geom_boxplot()
+
+## ---- col-movies
+movies %>% 
+  mutate(Major_Genre = fct_infreq(
+    Major_Genre)) %>% 
+  ggplot(aes(y = Major_Genre)) +
+  geom_bar()
+
+## ---- col-movies-lump
+movies %>% 
+  mutate(Major_Genre = fct_infreq(
+    fct_lump(Major_Genre, n = 6))) %>% #<<
+  ggplot(aes(y = Major_Genre)) +
+  geom_bar()
 
 ## ---- scores-sim
 set.seed(220)
@@ -63,61 +97,34 @@ scores_tbl
 ## ---- hist
 scores_tbl %>% 
   ggplot(aes(x = score)) +
-  geom_histogram()
+  geom_histogram() +
+  geom_vline(xintercept = 70, 
+    colour = "red")
 
 ## ---- cut
+(rng <- c(0, seq(39, 89, by = 5), 100))
 scores_tbl %>% 
-  mutate(
-    range = cut(score, breaks = c(0, seq(39, 89, by = 5), 100), 
-      include.lowest = TRUE))
+  mutate(range = cut(score, breaks = rng, include.lowest = TRUE))
 
 ## ---- recode
-scores_more <- scores_tbl %>% 
+scores_schemes <- scores_tbl %>% 
   mutate(
-    range = cut(score, breaks = c(0, seq(39, 89, by = 5), 100), 
-      include.lowest = TRUE),
-    grade = fct_recode(range,
+    range = cut(score, breaks = rng, include.lowest = TRUE),
+    grade = fct_recode(range, # new_lvl = old_lvl
       "D-" = "[0,39]", "D" = "(39,44]", "D+" = "(44,49]",
       "C-" = "(49,54]", "C" = "(54,59]", "C+" = "(59,64]",
       "B-" = "(64,69]", "B" = "(69,74]", "B+" = "(74,79]",
       "A-" = "(79,84]", "A" = "(84,89]", "A+" = "(89,100]"))
-scores_more
+scores_schemes
 
 ## ---- bar-range
-scores_more %>% 
+scores_schemes %>% 
   ggplot(aes(x = range)) +
   geom_bar()
 
 ## ---- bar-grade
-scores_more %>% 
+scores_schemes %>% 
   ggplot(aes(x = grade)) +
-  geom_bar()
-
-## ---- box-movies
-movies <- as_tibble(jsonlite::read_json(
-  "https://vega.github.io/vega-editor/app/data/movies.json",
-  simplifyVector = TRUE))
-ggplot(movies, aes(Rotten_Tomatoes_Rating, Major_Genre)) +
-  geom_boxplot()
-
-## ---- box-movies-med
-movies %>% 
-  mutate(
-    Major_Genre = fct_reorder(Major_Genre, Rotten_Tomatoes_Rating,
-      .fun = median, na.rm = TRUE)) %>% 
-  ggplot(aes(Rotten_Tomatoes_Rating, Major_Genre)) +
-  geom_boxplot()
-
-## ---- col-movies
-movies %>% 
-  mutate(Major_Genre = fct_infreq(Major_Genre)) %>% 
-  ggplot(aes(y = Major_Genre)) +
-  geom_bar()
-
-## ---- col-movies-lump
-movies %>% 
-  mutate(Major_Genre = fct_infreq(fct_lump_n(Major_Genre, 6))) %>% 
-  ggplot(aes(y = Major_Genre)) +
   geom_bar()
 
 ## ---- lubridate
@@ -127,39 +134,52 @@ library(lubridate)
 (td <- today())
 class(td)
 typeof(td)
-as.integer(td)
+as.integer(td) # 1970-01-01
 
 ## ---- dttm
 (current <- now())
 class(current)
 typeof(current)
-as.integer(current)
-
-## ---- parse-dttm
-ymd(c("2021/03/31", "1921-Jan-21"))
-ymd_h(c("2021-03-31 16", "2021-June-04 10"))
-ymd_h(c("2021-03-31 16", "2021-June-04 10"), tz = "Pacific/Auckland")
-as_date(18713)
-as_datetime(1616810967)
-
-## ---- tz-name
-set.seed(220)
-OlsonNames()[sample(1:length(OlsonNames()), 20)]
+as.integer(current) # 1970-01-01
 
 ## ---- create-dttm
 make_date(2021, c(3, 6), c(31, 4))
 make_datetime(2021, c(3, 6), c(31, 4), c(16, 10))
+make_datetime(2021, c(3, 6), c(31, 4), c(16, 10), tz = "Pacific/Auckland")
+
+## ---- tz-name
+set.seed(220)
+OlsonNames()[sample(1:length(OlsonNames()), 32)]
+
+## ---- parse-dttm
+ymd(c("2021/03/31", "2021-June-04"))
+ymd_h(c("2021-03-31 16", "2021-June-04 10"))
 dttm <- ymd_h(c("2021-03-31 16", "2021-June-04 10"), tz = "Pacific/Auckland")
 dttm
+
+## ---- convert-dttm
+# origin: 1970-01-01
+as_date(18713)
+as_datetime(1616810967) 
 
 ## ---- extract
 date(dttm)
 year(dttm)
+yday(dttm)
+week(dttm)
+
+## ---- extract2
+day(dttm) # mday(dttm)
+hour(dttm)
+minute(dttm)
+second(dttm)
+
+## ---- extract-mth
 month(dttm)
 month(dttm, label = TRUE)
-day(dttm) # mday(dttm)
-yday(dttm)
-wday(dttm)
+
+## ---- extract-wday
+wday(dttm, week_start = 1)
 wday(dttm, label = TRUE)
 wday(dttm, label = TRUE, week_start = 1)
 
@@ -172,12 +192,36 @@ round_date(dttm, "3 months")
 dttm + 1
 dttm + minutes(2)
 dttm + hours(3)
+
+## ---- arith1
 dttm + days(4)
+
+## ---- arith2
 dttm + weeks(5)
+
+## ---- arith3
 dttm + months(6)
+
+## ---- arith4
 dttm + years(7)
 
-## ---- misc
-# data example
+## ---- fmt
+format(dttm)
+format(dttm, "%Y/%b/%d")
+format(dttm, "%y/%b/%d %H:%M:%S")
+format(dttm, "on %d %B (%a)")
 
-# scale_x_date()
+## ---- release-date-chr
+movies$Release_Date[38:39]
+
+## ---- release-date
+movies %>% 
+  mutate(
+    Release_Date = parse_date_time( #<<
+      Release_Date, c("%d-%b-%y", "%Y-%m-%d")), #<<
+    Year = year(Release_Date) #<<
+  ) %>% 
+  filter(Year < 2012) %>% 
+  ggplot(aes(Year, IMDB_Rating)) +
+  geom_hex()
+
