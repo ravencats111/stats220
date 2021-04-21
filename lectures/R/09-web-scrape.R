35e0aaf7ac4eceba20bd5bdc335e7b45ed907162
earowang <earo.wang@gmail.com>
2021-04-21 21:45:05 +1200

    added more on rest api

--- lectures/R/09-web-scrape.R
+++ lectures/R/09-web-scrape.R
@@ -50,44 +50,65 @@ stats220 %>%
   html_children() %>%
   html_text2()
 
-## ---- dl
-stats220 %>% 
-  html_elements(".panel-body .btn") %>% 
-  html_attr("href") %>% 
-  url_absolute(home)
-
-stats220_urls <- stats220 %>% 
-  html_elements(".panel-body .btn") %>% 
-  html_attr("href") %>% 
-  url_absolute(home)
-
-pdf_urls <- stats220_urls[stringr::str_detect(stats220_urls, "pdf")]
-pdf_ursl %>% 
-  map(download.file)
-
+## ---- html-info
 stats220_info <- read_html("https://stats220.earo.me/pages/info/")
 
+## ---- html-h3
 stats220_info %>% 
   html_elements("h3") %>% 
   html_text()
 
+## ---- html-id
 stats220_info %>% 
   html_elements("#timetable") %>% 
   html_text()
 
+## ---- html-table
 stats220_info %>% 
   html_element("table") %>% 
   html_table()
 
-# https://docs.github.com/en/rest
+## ---- links
+stats220_urls <- stats220 %>% 
+  html_elements(".panel-body .btn") %>% 
+  html_attr("href") %>% 
+  url_absolute(home)
+stats220_urls
+
+## ---- pdf-links
+(pdf_urls <- stats220_urls[stringr::str_detect(stats220_urls, "pdf")])
+
+## ---- pdf-links-dl
+pdf_ursl %>% 
+  map(download.file)
+
+## ---- root-endpoint
 library(httr)
+endpoint <- "https://api.github.com"
+GET(endpoint)
+
+## ---- endpoint
+path <- "/repos/STATS-UOA/stats220"
+resp <- GET(modify_url(endpoint, path = path))
+resp
+
+## ---- content-type
+http_type(resp)
+content(resp)
+
+## ---- status
+status_code(resp)
+http_status(200)
+http_status(404)
+
+## ---- commits
+path_commits <- "/repos/STATS-UOA/stats220/commits"
+resp_commits <- GET(modify_url(endpoint, path = path_commits))
+dat <- content(resp_commits)
+dat
+
+## ---- commits-dttm
 library(lubridate)
 library(tidyverse)
-stats220_gh <- "https://api.github.com/repos/STATS-UOA/stats220/commits"
-resp <- GET(stats220_gh)
-status_code(resp)
-
-lst <- content(resp)
-dttm_chr <- map_chr(lst, ~ .$commit$committer$date)
-as_datetime(dttm_chr)
+dttm_chr <- map_chr(dat, ~ .$commit$committer$date)
 as_datetime(dttm_chr, tz = "Pacific/Auckland")
